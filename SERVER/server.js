7237e794b288040c0e059b7c0ad1de28486e3598
jakub <jakub@bryntum.com>
2013-06-18 03:36:22 +0200

    - removed socket token (using id seems safe) - added joining rooms - added Player class

--- SERVER/server.js
+++ SERVER/server.js
@@ -21,21 +21,6 @@ function main (req, res) {
     });
 }
 
-// var Player = function(data){
-
-//     var setPos = function(x, y, z){
-
-//     }
-
-//     return {
-
-//     }
-// };
-
-var generateSocketToken = function(){
-    return crypto.randomBytes(20).toString('hex');
-};
-
 var generateGameHash = function(){
     var seed = crypto.randomBytes(20),
         //we leave the first 6 chars of the hash 
@@ -49,39 +34,77 @@ var generateGameHash = function(){
     return hash;
 };
 
+var Player = function(id){
+    this.id = id;
+    this.x = null;
+    this.y = null;
+    this.z = null;
 
+    var me = this;
+
+    var setPos = function(x, y, z){
+        me.x = x;
+        me.y = y;
+        me.z = z;
+    };
+
+    var getId = function(){
+        return me.id;
+    };
+
+    return {
+        setPos : setPos,
+        getId : getId
+    };
+};
+
+//all game players
 MZ.PLAYERS = {};
+//object with all games running (pvp) - games also act as rooms for broadcasting messages
 MZ.GAMES = {};
-MZ.NEW_PLAYERS = [];
+//object with connected players not in game
+MZ.NEW_PLAYERS = {};
 //object with all sockets currently connected
 MZ.SOCKETS = {};
 
 io.sockets.on('connection', function (socket) {
-    
-    socket.token = generateSocketToken();
-    MZ.SOCKETS[socket.token] = socket;
+    var token  = socket.id,
+        player = new Player(token);
+
+    MZ.SOCKETS[token] = socket;
+    MZ.NEW_PLAYERS[token] = player;
 
     //events with front/back prefix for easier development
-    socket.emit('back-connected', { socketToken: socket.token });
+    socket.emit('back-connected', { socketToken: token });
 
     socket.on('front-newgame', function() {
-        var hash = generateGameHash();
+        var hash = generateGameHash(),
+            player = MZ.NEW_PLAYERS[socket.id];
 
-        //przypisac hash do playera
-        //usunac playera z new_players
-        //dodac gre do games
-        MZ.GAMES[hash] = [];
+        MZ.PLAYERS[player.getId()] = player;
+        MZ.GAMES[hash] = [player];
+        delete MZ.NEW_PLAYERS[player.getId()];
 
-        socket.emit('back-newgame', {hash: hash});
+        //join to new room
+        //https://github.com/LearnBoost/socket.io/wiki/Rooms
+        socket.join(hash);
+
+        //debug
+        var rooms = io.sockets.manager.roomClients[socket.id];
+
+        socket.emit('back-newgame', {hash: hash, rooms: rooms});
     });
+
+    //TODO events for second player joining game needed
 
     socket.on('front-playermoved', function(data){
         socket.emit('back-playermoved');
     });
 
     socket.on('disconnect', function () {
-        delete MZ.SOCKETS[socket.token];
+        delete MZ.SOCKETS[socket.id];
+        delete MZ.PLAYERS[socket.id];
 
-        //socket.broadcast.emit('back-playerleft');
+        //TODO if no player left in room, delete game
     });
 });
