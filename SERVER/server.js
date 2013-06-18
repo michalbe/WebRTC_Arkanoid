c0d8f71c3d5306a3a089a59a114633779d23a579
jakub <jakub@bryntum.com>
2013-06-19 00:25:04 +0200

    - socketio events now use rooms - docs - some helper fn

--- SERVER/server.js
+++ SERVER/server.js
@@ -39,6 +39,7 @@ var Player = function(id){
     this.x = null;
     this.y = null;
     this.z = null;
+    this.game = null;
 
     var me = this;
 
@@ -52,17 +53,49 @@ var Player = function(id){
         return me.id;
     };
 
+    var joinGame = function(gameHash){
+        this.game = gameHash;
+    };
+
+    var getGame = function(){
+        return this.game;
+    };
+
     return {
         setPos : setPos,
-        getId : getId
+        getId : getId,
+        joinGame : joinGame,
+        getGame : getGame
     };
+};
+
+var removeGamePlayer = function(gameId, player){
+    var game = MZ.GAMES[gameId],
+        idx = null;
+
+    for (var i=0, l=game.length; i<l; i+=1){
+        if (game[i] === player){
+            idx = i;
+            break;
+        }
+    }
+
+    if (idx != null){
+        game.splice(idx, 1);
+    }
 };
 
 //all game players
 MZ.PLAYERS = {};
-//object with all games running (pvp) - games also act as rooms for broadcasting messages
+
+/*object with all games running (pvp) - games also act as rooms for broadcasting messages
+ so that we can easily select players playing together. The structure is :
+ {
+    'gameHash' : [ player1, player2 ]
+ }
+*/
 MZ.GAMES = {};
-//object with connected players not in game
+//object with connected players not currently in game
 MZ.NEW_PLAYERS = {};
 //object with all sockets currently connected
 MZ.SOCKETS = {};
@@ -82,7 +115,7 @@ io.sockets.on('connection', function (socket) {
             player = MZ.NEW_PLAYERS[socket.id];
 
         MZ.PLAYERS[player.getId()] = player;
-        MZ.GAMES[hash] = [player];
+        MZ.GAMES[hash] = [socket];
         delete MZ.NEW_PLAYERS[player.getId()];
 
         //join to new room
@@ -98,13 +131,27 @@ io.sockets.on('connection', function (socket) {
     //TODO events for second player joining game needed
 
     socket.on('front-playermoved', function(data){
-        socket.emit('back-playermoved');
+        var player = MZ.PLAYERS[socket.id],
+            gameHash = player.getGame();
+
+        socket.broadcast.to(gameHash).emit('back-playermoved');
     });
 
     socket.on('disconnect', function () {
-        delete MZ.SOCKETS[socket.id];
-        delete MZ.PLAYERS[socket.id];
+        var player = MZ.PLAYERS[socket.id],
+            gameHash = player.getGame(),
+            game   = MZ.GAMES[gameHash];
 
-        //TODO if no player left in room, delete game
+        removeGamePlayer(game, player);
+
+        //second player still in the game
+        if (game.length > 0){
+            socket.broadcast.to(gameHash).emit('back-playerleft');
+        } else {
+            delete MZ.GAMES[player.getGame()];
+        }
+
+        delete MZ.SOCKETS[socket.id];
+        delete player;
     });
 });
